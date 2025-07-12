c100081d5129c1fe9aee8c5b23b19cbe5e5ac974
0097eo <emmanuelokello294@gmail.com>
2025-07-12 09:45:39 +0300

    Add TaskAssignedMail class and update Task model with relationships and fillable attributes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'deadline',
+        'user_id',
+        'status',
+    ];
+
+    /**
+     * Get the user that the task is assigned to.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
