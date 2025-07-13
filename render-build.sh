30bce7359cbe92e203d9d4cdd107d1e9c03aae34
0097eo <emmanuelokello294@gmail.com>
2025-07-13 12:15:29 +0300

    fix: Install Node.js in Render build script

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,14 @@
 # Exit on error
 set -o errexit
 
+# Install PHP dependencies
 echo "Running Composer Install..."
 composer install --no-dev --no-interaction --optimize-autoloader
+
+echo "Installing Node.js..."
+source /opt/render/etc/profile.d/nvm.sh
+nvm install 18
+nvm use 18
 
 echo "Installing NPM dependencies..."
 npm install
